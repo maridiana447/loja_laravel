214e8db93e350f09391f0b61958a7a44abb3e1aa
unknown <vitin.ferreira3022@.gmail.com>
2026-05-26 22:40:04 -0300

    tudo novo

--- novo/public/cadastro_produto.js
+++ novo/public/cadastro_produto.js
@@ -1,35 +1,30 @@
 $(document).ready(function () {
+    $.ajaxSetup({
+        headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        }
+    });
+
     $("#btn_cadastrar").click(function () {
         $.ajax({
             type: "POST",
-            url: "api/salvar_produto",
+            url: "/api/salvar_produto",
             data: {
-                nome_produto: $("#nome_produto").val(),
-                descricao_produto: $("#descricao_produto").val(),
-                preco_produto: $("#preco_produto").val(),
-                estoque_produto: $("#estoque_produto").val(),
-                tipo_produto: $("#tipo_produto").val(),
-                data_validade: $("#data_validade").val(),
+                nome_produto:      $("#nome_produto").val(),
+                telefone_produto:  $("#telefone_produto").val(),
+                email_produto:     $("#email_produto").val(),
+                data_nascimento:   $("#data_nascimento").val(),
             },
             dataType: "JSON",
             success: function (data) {
-                console.log(data);
                 if (data.erro == 'n') {
-                    Swal.fire({
-                        title: "Ótimo!",
-                        text: "Produto cadastrado!",
-                        icon: "success"
-                    });
-                
+                    Swal.fire({ title: "Ótimo!", text: data.msg, icon: "success" });
                 } else {
-                    Swal.fire({
-                        title: "Vish!",
-                        text: "Deu ruim ",
-                        icon: "error"
-                    });
+                    Swal.fire({ title: "Erro!", text: data.msg, icon: "error" });
                 }
-
-
+            },
+            error: function (xhr) {
+                Swal.fire({ title: "Erro!", text: "Status " + xhr.status + " - veja o console.", icon: "error" });
             }
         });
     });
